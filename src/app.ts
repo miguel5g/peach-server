c5d4af5374c7977a8c50dfcfbfbd29893b326b03
Miguel Ângelo <miguelcg958@gmail.com>
2021-12-03 23:18:58 -0300

    added static files middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import path from "path";
 import { Server } from "socket.io";
 import { createServer } from "http";
+
 import { routes } from "./routes";
 
 const app = express();
@@ -12,6 +14,7 @@ const io = new Server(server);
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "..", "public")));
 app.use(routes);
 
 export { server, io };
